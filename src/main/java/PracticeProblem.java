0e33648ae46d6eca1167b91e1f276390dc85ec51
cindy pan <cindy.pan@student.tdsb.on.ca>
2025-04-07 01:01:26 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,20 @@
+
 public class PracticeProblem {
+	public static int towerOfHanoi(int num){
 
-	public static void main(String args[]) {
-
+		if (num<3){
+			return -1;
+		}
+		if (num==3){
+			return 7;
+		}
+		else{
+			
+			return 2 * towerOfHanoi(num-1) +1;
+			// 1 -> b 2->c 1-> c (3 moves)
+			// 3 -> b 1->a 2 -> b 1-> b (4 moves) 
+			// 7 moves to move layer 3 (2*3+1)
+		}
 	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+	
 }
